b69d9fe5f068732426df75695873e3101c64458e
AndresMora08 <pipemora0101@gmail.com>
2026-06-04 13:22:33 -0500

    firebase configurado

--- src/app/services/environments/environment.ts
+++ src/app/services/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5000/api'
+  apiUrl: 'http://127.0.0.1:5000', // Tu API de Flask actual
+  firebaseConfig: {
+    apiKey: "AIzaSyAJTedQWQmuhfvRI8W6gmaHqKKlqg8xKrU",
+    authDomain: "proyecto-angular-dab35.firebaseapp.com",
+    projectId: "proyecto-angular-dab35",
+    storageBucket: "proyecto-angular-dab35.firebasestorage.app",
+    messagingSenderId: "859669609740",
+    appId: "1:859669609740:web:221ce2f9c6c7d5e5eb9d29",
+    measurementId: "G-63ZR8P8KR8"
+  }
 };
